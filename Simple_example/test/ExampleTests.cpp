1e645fb10c528f391322a18d109bf375249162d8
Juan Jose Jurado Camino <juan-jose.jurado@elecnor.es>
2021-11-02 12:09:11 +0100

    One change in the exampleTests.cpp

--- Simple_example/test/ExampleTests.cpp
+++ Simple_example/test/ExampleTests.cpp
@@ -1,5 +1,11 @@
 #include <gtest/gtest.h>
 
 TEST(ExampleTests, DemonstrateGTestMacros){
-    EXPECT_TRUE(false);
+    EXPECT_TRUE(true);
+    EXPECT_EQ(true, true);
+    EXPECT_EQ(true, false);
+}
+
+TEST(ExampleTests, Demonstrate){
+    EXPECT_TRUE(true);
 }
